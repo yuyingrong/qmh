680adc3d81618e7f4481c7ec48a08d8723c40d44
Mike Zarafonetis <mzarafon@haverford.edu>
2019-10-01 15:07:30 +0000

    Andy's changes to the patient map and admin

--- myAsylum/qmh/admin.py
+++ myAsylum/qmh/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from qmh.models import *
 # Register your models here.
+
+class PatientAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Patient, PatientAdmin)
